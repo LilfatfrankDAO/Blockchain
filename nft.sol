8661c696ed918b2c0635d01cb088eee326fe37ba
yatharth-zebpay <yatharth.a@zebpay.com>
2022-04-09 14:11:48 +0530

    ERC721 minting

---
+++ nft.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LilfatfrankDAO is ERC721URIStorage, Ownable {
+    
+    uint256 public tokenCounter;
+
+    constructor()  ERC721("lilfatfrank", "LFF") {
+        tokenCounter = 0;
+    }
+
+
+    function createCollectible(string memory tokenURI,address to) public onlyOwner returns (uint256) {
+        uint256 newItemid = tokenCounter;
+        _safeMint(msg.sender, newItemid);
+        _setTokenURI(newItemid,tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newItemid;
+    }
+
+}
